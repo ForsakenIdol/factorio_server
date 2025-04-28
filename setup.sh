c8a4c95c61c7db4264f4e59abd2339af1c7bad4c
EC2 Default User <ec2-user@ip-172-31-35-233.ap-southeast-1.compute.internal>
2025-04-28 14:40:23 +0000

    Merged mod setup script into main setup script

--- setup.sh
+++ setup.sh
@@ -24,5 +24,20 @@ echo "Server unpacked. Ready to start headless server instance.";
 # Let's use a test save file that would be generated using the command below in the same directory as the setup script.
 # ./factorio/factorio/bin/x64/factorio --create ./saves/test-save.zip
 
-# Save file has been created, all that is left to do is to start the server.
+cd ..
 
+# Time to load mods.
+echo "Time to load mods. We need to start and stop the server once to create the mods/ folder...";
+# Piping stdout to null prevents it from crashing with an empty STDIN error.
+./factorio/factorio/bin/x64/factorio --start-server ./saves/test-save.zip --server-settings ./server-settings.json 1>/dev/null &
+FACTORIO_PID=$!
+echo "Factorio process sent to background. PID $FACTORIO_PID...";
+sleep 5;
+echo "Stopping server...";
+kill $FACTORIO_PID;
+
+# First, download the mods as a zip.
+echo "Downloading mod zip...";
+curl "https://cdn.discordapp.com/attachments/427068629486534667/1366410416057618564/factorio_mods.zip?ex=6810d85d&is=680f86dd&hm=777e92924a7a367050e37c7153f1f5d19dbad28f40eadcce48d5769bff8ef1b4&" --output factorio_mods.zip;
+echo "Mod zip downloaded, unzipping...";
+unzip -o ./factorio_mods.zip -d ./factorio/factorio/mods/;
